7f6bbc322cc7567a5c65f238002500c519f65405
ellcylalu <ellcyhenry.lalu.iics@ust.edu.ph>
2022-10-17 19:11:15 +0800

    Add project files.

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UsteQuake.Server.Data;
+using UsteQuake.Server.Models;
+
+[assembly: HostingStartup(typeof(UsteQuake.Server.Areas.Identity.IdentityHostingStartup))]
+namespace UsteQuake.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
